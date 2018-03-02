4d5b98604f46da04ca0fcf33daa7ae1a8da75c57
t-ex-140542 <wellington5925@hotmail.com>
2018-03-02 12:01:03 -0300

    alteraçoes no layout cores e bottomnavigation

--- app/src/main/java/com/example/t_ex_140542/projetongithub/MainActivity.java
+++ app/src/main/java/com/example/t_ex_140542/projetongithub/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.t_ex_140542.projetongithub;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_recentes:
+                        Toast toast = Toast.makeText(MainActivity.this, "Clique aqui para recentes", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+                        toast.show();
+
+                        break;
+                    case R.id.action_favoritos:
+                        Toast toast1 = Toast.makeText(MainActivity.this, "Clique aqui para recentes", Toast.LENGTH_LONG);
+                        toast1.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+                        toast1.show();
+
+                        break;
+                    case R.id.action_Proximidades:
+                        Toast toast2 = Toast.makeText(MainActivity.this, "Clique aqui para recentes", Toast.LENGTH_LONG);
+                        toast2.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+                        toast2.show();
+
+                        break;
+
+                }
+                return true;
+            }
+
+        });
     }
 }
